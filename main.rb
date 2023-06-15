eb232def34d2686d35e3ed00be310f14fe346db7
Patrick Mukula <Patrickkeita80@yahoo.com>
2023-06-15 07:36:45 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -28,19 +28,17 @@ morse_code = {
 }
 
 def decode_char(string, morse_code)
-  print morse_code[string]
+   morse_code[string]
 end
 
 def decode_word(morse, morse_code)
-  print "#{morse.split.map { |char| decode_char(char, morse_code) }.join} "
+   morse.split.map { |char| decode_char(char, morse_code) }.join
 end
 
 def decode_message(message, morse_code)
-  print message.split('   ').map { |word| decode_word(word, morse_code) }.join(' ')
+  message.split('   ').map { |word| decode_word(word, morse_code) }.join(' ')
 end
 
 decode_char('.-', morse_code)
-print(' ')
 decode_word('-- -.--', morse_code)
-print(' ')
 decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...', morse_code)
