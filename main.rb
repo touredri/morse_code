c15c35209bbaf5755d861b07ee6f1d7c5f798745
touredri <dcompte223@gmail.com>
2023-06-15 11:54:14 +0100

    Setup & added methods

---
+++ main.rb
@@ -0,0 +1,41 @@
+morse_code = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(string, morse_code)
+  puts morse_code[string]
+end
+
+def decode_word(morse, morse_code)
+  decoded_chars = morse.split.map { |char| morse_code[char] }
+  decoded_word = decoded_chars.join
+  puts decoded_word
+end
+
+decode_char('.-', morse_code)
+decode_word('-- -.--', morse_code)
